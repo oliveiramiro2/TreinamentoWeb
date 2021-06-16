39928c948812b1114a8a978a55388b487732ac2c
oliveiramiro2 <ramiromares95@gmail.com>
2021-06-16 16:08:40 -0300

    configurando a tarefa dentro do admin

--- configToDo/ToDo/models.py
+++ configToDo/ToDo/models.py
@@ -3,15 +3,18 @@ from django.db import models
 class Task(models.Model):
     
     STATUS = (
-        ('doing', 'Doing'),
-        ('done', 'Done'),
+        ("1", 'Doing'),
+        ("2", 'Done'),
     )
     
     title = models.CharField(max_length=255)
     description = models.TextField()
     done = models.CharField(
-        max_length=5,
+        max_length=8,
         choices=STATUS,
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
