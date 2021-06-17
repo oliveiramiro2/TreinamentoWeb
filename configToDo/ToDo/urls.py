b5179cd21d51bbb5ad291c04d24b1a35f371402a
oliveiramiro2 <ramiromares95@gmail.com>
2021-06-17 14:37:51 -0300

    mostrando tarefas na tela

--- configToDo/ToDo/urls.py
+++ configToDo/ToDo/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homePage)
+    path('', views.homePage, name='task-list'),
+    path('tasks/<int:id>', views.taskView, name='task-view'),
 ]
--- configToDo/ToDo/views.py
+++ configToDo/ToDo/views.py
@@ -1,7 +1,11 @@
 from django.db import models
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Task
 
 def homePage(request):
     tasks = Task.objects.all()
     return render(request, 'todo/index.html', {'tasks': tasks})
+
+def taskView(request, id):
+    task = get_object_or_404(Task, pk=id)
+    return render(request, 'todo/task.html', {'task': task})
