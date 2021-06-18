cd48c0ac9778aa083cd24cf43de4c43dee21935e
oliveiramiro2 <ramiromares95@gmail.com>
2021-06-18 00:26:23 -0300

    arrumando bug no editar

--- configToDo/ToDo/urls.py
+++ configToDo/ToDo/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.homePage, name='task-list'),
     path('tasks/<int:id>', views.taskView, name='task-view'),
     path('newtask/', views.newTask, name='new-task'),
+    path('edit/<int:id>', views.editTask, name='edit-task'),
 ]
