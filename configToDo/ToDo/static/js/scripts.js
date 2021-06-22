b6cdedd5eeac26d200654e41649fc202853e9d2d
oliveiramiro2 <ramiromares95@gmail.com>
2021-06-22 07:00:55 -0300

    arrumando o filtro porem bugs no js ainda

--- configToDo/ToDo/static/js/scripts.js
+++ configToDo/ToDo/static/js/scripts.js
@@ -2,8 +2,17 @@ $(document).ready(() => {
     //search 
     const searchBtn = $('#search-btn')
     const searchForm = $('#search-form')
+    //url base e filtro
+    const baseUrl = 'http://localhost:8000/'
+    const filter = $('#filter')
 
+    //search 
     $(searchBtn).on('click', function() {
         searchForm.submit()
     })
+
+    //filtro
+    $(filter).change(this).val()
+    console.log(filter)
+    window.location.href = baseUrl + '?filter=', filter
 })
--- configToDo/ToDo/views.py
+++ configToDo/ToDo/views.py
@@ -9,9 +9,13 @@ from .models import Task
 @login_required
 def homePage(request):
     search = request.GET.get("search")
+    filter = request.GET.get("filter")
+    
     if search:
         #filtro
         tasks = Task.objects.filter(title__icontains=search, user=request.user)
+    elif filter:
+        tasks = Task.objects.filter(done=filter, user=request.user)
     else:
         #tarefas e paginacao sem filto
         tasks_list = Task.objects.all().order_by('-created_at').filter(user=request.user)
